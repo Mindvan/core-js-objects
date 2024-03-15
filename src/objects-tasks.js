15040cb12465474c71f62bfc1ce4d1c3d7716ec6
Ivan Eroshin <itseroshin@gmail.com>
2024-03-16 00:21:42 +0300

    feat: more solutions solved up to makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,22 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const result = {};
+
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key]) => {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        if (Object.prototype.hasOwnProperty.call(result, key)) {
+          result[key] += obj[key];
+        } else {
+          result[key] = obj[key];
+        }
+      }
+    });
+  });
+
+  return result;
 }
 
 /**
@@ -50,8 +64,16 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const object = obj;
+
+  Object.keys(object).forEach((key) => {
+    if (keys.includes(key)) {
+      delete object[key];
+    }
+  });
+
+  return object;
 }
 
 /**
@@ -66,8 +88,8 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -81,8 +103,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length <= 0;
 }
 
 /**
@@ -101,8 +123,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -115,8 +137,17 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  let str = '';
+
+  const { length } = Object.values(lettersObject).flat(Infinity);
+  for (let i = 0; i < length; i += 1) {
+    str += Object.keys(lettersObject).find((key) =>
+      lettersObject[key].includes(i)
+    );
+  }
+
+  return str;
 }
 
 /**
